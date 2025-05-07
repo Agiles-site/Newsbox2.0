08f04a898191befc2ea9ec3124ecacbfa9c73b2e
adnan-borshon <adnan.borshon@gmail.com>
2025-05-07 20:19:53 +0600

    addded

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "../styles/globals.css";
+import "./globals.css";
 import Header from "@/components/layout/header"
 import Footer from "@/components/layout/footer"
 import { ThemeProvider } from "@/components/theme-provider"
